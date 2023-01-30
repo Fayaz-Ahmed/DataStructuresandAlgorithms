f3649577ca14bd894a392b462681cfb3ac8b907d
Fayaz-Ahmed <fayaz861818@gmail.com>
2023-01-30 09:53:26 +0530

    Find First Bad Version

---
+++ src/Practise/FirstBadVersion.java
@@ -0,0 +1,15 @@
+package Practise;
+
+public class FirstBadVersion {
+}
+
+// Public class Solution extends VersionControl {
+//    public int firstBadVersion(int n) {
+//        for(int i=0;i<n;i++){
+//            if(isBadVersion(i)==true){
+//                return i;
+//            }
+//        }
+//        return 1;
+//    }
+//}
